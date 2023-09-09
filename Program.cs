b101d350bc8e97a4b95088a89149164ecf460907
Agile101hcmue <89836403+TrHgTung@users.noreply.github.com>
2023-09-09 19:58:00 +0700

    Added Entity Framework and its components

--- Program.cs
+++ Program.cs
@@ -1,4 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using WebApp01.Repository;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Connection DB
+builder.Services.AddDbContext<DataContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration["ConnectionStrings:ConnectedDb"]);
+});
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
---
+++ Repository/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApp01.Models;
+
+namespace WebApp01.Repository
+{
+    public class DataContext : DbContext
+
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) 
+        { 
+            //
+        }
+        public DbSet<BrandModel> Brands { get; set; }
+        public DbSet<ProductModel> Products { get; set; }
+        public DbSet<CategoryModel> Categories { get; set; }
+    }
+}
