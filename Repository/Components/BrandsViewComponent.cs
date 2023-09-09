e89c485ae686b68907bb06ff6bed41c182680c41
Agile101hcmue <89836403+TrHgTung@users.noreply.github.com>
2023-09-10 01:44:50 +0700

    Show Categories & Brands (on Sidebar)

---
+++ Repository/Components/BrandsViewComponent.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApp01.Repository.Components
+{
+    public class BrandsViewComponent : ViewComponent
+    {
+        private readonly DataContext _dataContext;
+        public BrandsViewComponent(DataContext context)
+        {
+            _dataContext = context;
+        }
+        public async Task<IViewComponentResult> InvokeAsync() => View(await _dataContext.Brands.ToListAsync());
+    }
+}
---
+++ Repository/Components/CategoriesViewComponent.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApp01.Repository.Components
+{
+    public class CategoriesViewComponent : ViewComponent
+    {
+        private readonly DataContext _dataContext;
+        public CategoriesViewComponent(DataContext context)
+        {
+            _dataContext = context;
+        }
+        public async Task<IViewComponentResult> InvokeAsync() => View(await _dataContext.Categories.ToListAsync());
+    }
+}
