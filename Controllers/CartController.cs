20dc9507d34d0d79109dbd08c8540793b90fce88
Agile101hcmue <89836403+TrHgTung@users.noreply.github.com>
2023-09-12 18:08:53 +0700

    Add Cart

--- Controllers/CartController.cs
+++ Controllers/CartController.cs
@@ -1,12 +1,26 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WebApp01.Models;
+using WebApp01.Models.ViewModels;
+using WebApp01.Repository;
 
 namespace WebApp01.Controllers
 {
     public class CartController : Controller
     {
+        private readonly DataContext _dataContext;
+        public CartController (DataContext _context)
+        {
+            _dataContext = _context;
+        }
         public ActionResult Index()
         {
-            return View();
+            List<CartItemModel> cartItems = HttpContext.Session.GetJson<List<CartItemModel>>("Cart") ?? new List<CartItemModel>();
+            CartItemViewModel cartVM = new()
+            {
+                CartItems = cartItems,
+                GrandTotal = cartItems.Sum(x => x.Price * x.Quantity),
+            };
+            return View(cartVM);
         }
 
         public ActionResult Checkout()
