35c75ecba81a6589bde4d4a0742b68c79fc1b200
Agile101hcmue <89836403+TrHgTung@users.noreply.github.com>
2023-09-12 13:53:58 +0700

    Show Products on Index

---
+++ Controllers/BrandController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApp01.Models;
+using WebApp01.Repository;
+
+namespace WebApp01.Controllers
+{
+	public class BrandController : Controller
+	{
+		private readonly DataContext _dataContext;
+		public BrandController(DataContext context)
+		{
+			_dataContext = context;
+		}
+		public async Task<IActionResult> Index(string Slug = "")
+		{
+			BrandModel brand = _dataContext.Brands.Where(c => c.Slug == Slug).FirstOrDefault();
+			if (brand == null)
+			{
+				return RedirectToAction("Index");
+			}
+			var productsByBrand = _dataContext.Products.Where(p => p.BrandId == brand.Id);
+			return View(await productsByBrand.OrderByDescending(p => p.Id).ToListAsync());
+		}
+	}
+}
--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -1,12 +1,26 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApp01.Models;
+using WebApp01.Repository;
 
 namespace WebApp01.Controllers
 {
     public class CategoryController : Controller
     {
-        public IActionResult Index()
+		private readonly DataContext _dataContext;
+		public CategoryController(DataContext context) 
+        { 
+            _dataContext = context; // lay du lieu
+        }
+		public async Task<IActionResult> Index(string Slug = "")
         {
-            return View();
+            CategoryModel category = _dataContext.Categories.Where(c =>  c.Slug == Slug).FirstOrDefault();
+            if (category == null)
+            {
+                return RedirectToAction("Index");
+            }
+            var productsByCategory = _dataContext.Products.Where(p => p.CategoryId == category.Id);
+            return View(await productsByCategory.OrderByDescending(p => p.Id).ToListAsync());
         }
 
       
