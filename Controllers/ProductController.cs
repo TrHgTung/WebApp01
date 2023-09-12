10003a94e4d85dddf0f6289af72464c26f506a3d
Agile101hcmue <89836403+TrHgTung@users.noreply.github.com>
2023-09-12 16:00:10 +0700

    Display product info

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -1,14 +1,25 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WebApp01.Repository;
 
 namespace WebApp01.Controllers
 {
     public class ProductController : Controller
     {
+        private readonly DataContext _dataContext;
+        public ProductController(DataContext context)
+        {
+            _dataContext = context; 
+        }
         public IActionResult Index() { 
             return View(); 
         }
-        public IActionResult Details() { 
-            return View(); 
+        public async Task<IActionResult> Details(int Id ) {
+            if(Id == null)
+            {
+                return RedirectToAction("Index");
+            }
+            var productById = _dataContext.Products.Where(p =>  p.Id == Id).FirstOrDefault();
+            return View(productById); 
         }
     }
 }
