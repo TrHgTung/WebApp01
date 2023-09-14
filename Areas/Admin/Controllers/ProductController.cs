9c39a02c60eda3de18117cb895cee0ace435487a
Agile101hcmue <89836403+TrHgTung@users.noreply.github.com>
2023-09-14 18:25:13 +0700

    'Add products' form

--- Areas/Admin/Controllers/ProductController.cs
+++ Areas/Admin/Controllers/ProductController.cs
@@ -11,9 +11,11 @@ namespace WebApp01.Areas.Admin.Controllers
     public class ProductController : Controller
     {
         private readonly DataContext _dataContext;
-        public ProductController(DataContext context)
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        public ProductController(DataContext context, IWebHostEnvironment webHostEnvironment)
         {
             _dataContext = context;
+            _webHostEnvironment = webHostEnvironment;
         }
         public async Task<IActionResult> Index()
         {
@@ -27,10 +29,57 @@ namespace WebApp01.Areas.Admin.Controllers
             return View();
         }
 
-        public async Task<IActionResult> CreateProduct(ProductModel product)
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(ProductModel product)
         {
             ViewBag.Categories = new SelectList(_dataContext.Categories, "Id", "Name", product.CategoryId);
             ViewBag.Brands = new SelectList(_dataContext.Brands, "Id", "Name", product.BrandId);
+            
+            if(ModelState.IsValid) // neu du lieu nhập vào đúng với Model
+            {
+                // them du lieu
+                //TempData["success"] = "OK";
+                product.Slug = product.Name.Replace(" ", "-");
+                var slug = await _dataContext.Products.FirstOrDefaultAsync(p => p.Slug == product.Slug);
+                if(slug != null)
+                {
+                    ModelState.AddModelError("", "Sản phẩm đã có sẵn");
+                    return View(product);
+                }
+                
+                    if(product.ImageUpload != null)
+                    {
+                        string uploadsDir = Path.Combine(_webHostEnvironment.WebRootPath, "images/products"); // thu muc luu hinh anh
+                        string imageName = Guid.NewGuid().ToString() + "_" + product.ImageUpload.FileName; // set tên mới cho hinh anh (random id + ten goc)
+                        string filePath = Path.Combine(uploadsDir, imageName); // đường dẫn lưu hình ảnh
+
+                        FileStream fs = new FileStream(filePath, FileMode.Create);
+                        await product.ImageUpload.CopyToAsync(fs); // copy hinh anh (1 file ảnh mới) vao thu muc
+                        fs.Close();
+
+                        product.Image = imageName;
+                    }
+               
+                _dataContext.Add(product);
+                await _dataContext.SaveChangesAsync();
+                TempData["success"] = "Đã thêm thành công";
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                TempData["error"] = "Có lỗi xảy ra";
+                List<string> errors = new List<string>();
+                foreach(var value in ModelState.Values)
+                {
+                    foreach(var error in value.Errors) {
+                        errors.Add(error.ErrorMessage);
+                    }
+                }
+                string errorMessage = string.Join("\n", errors);
+                return BadRequest(errorMessage);
+            }
+            
             return View(product);
         }
     }
