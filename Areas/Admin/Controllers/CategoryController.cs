d9704fc1dafd1316fa2a229eceae2171c51d758a
Agile101hcmue <89836403+TrHgTung@users.noreply.github.com>
2023-09-20 00:41:51 +0700

    Display Categories in admin

---
+++ Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApp01.Repository;
+
+namespace WebApp01.Areas.Admin.Controllers
+{
+	[Area("Admin")]
+	public class CategoryController : Controller
+	{
+		private readonly DataContext _dataContext;
+		public CategoryController(DataContext context) 
+		{
+			_dataContext = context;
+		}
+
+		public async Task<IActionResult> Index()
+		{
+			return View(await _dataContext.Categories.OrderByDescending(p => p.Id).ToListAsync());
+		}
+	}
+}
