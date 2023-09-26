7ccf778a6722076ab924ee3810b0683a1e65a115
Agile101hcmue <89836403+TrHgTung@users.noreply.github.com>
2023-09-26 13:39:34 +0700

    Added Confirmation before delete Product/Brand/Category

---
+++ wwwroot/Admin/js/site.js
@@ -0,0 +1,33 @@
+﻿$(function () {
+
+    if ($("a.confirmDeletion").length) {
+        $("a.confirmDeletion").click(() => {
+            if (!confirm("Xác nhận xóa?")) return false;
+        });
+    }
+
+    if ($("a.confirmImportantDeletion").length) {
+        $("a.confirmImportantDeletion").click(() => {
+            if (!confirm("Lưu ý! Thao tác xóa này có thể gây ảnh hưởng đến các sản phẩm thuộc danh mục / NXB. Vẫn muốn xóa?")) return false;
+        });
+    }
+
+    if ($("div.alert.notification").length) {
+        setTimeout(() => {
+            $("div.alert.notification").fadeOut();
+        }, 2000);
+    }
+
+});
+
+function readURL(input) {
+    if (input.files && input.files[0]) {
+        let reader = new FileReader();
+
+        reader.onload = function (e) {
+            $("img#imgpreview").attr("src", e.target.result).width(200).height(200);
+        };
+
+        reader.readAsDataURL(input.files[0]);
+    }
+}
