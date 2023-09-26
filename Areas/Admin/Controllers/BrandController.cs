f8a2e460a628934f43e7189dc418ddc8576185c2
Agile101hcmue <89836403+TrHgTung@users.noreply.github.com>
2023-09-26 11:07:40 +0700

    Finish CRUD for BrandProduct

---
+++ Areas/Admin/Controllers/BrandController.cs
@@ -0,0 +1,128 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using WebApp01.Models;
+using WebApp01.Repository;
+
+namespace WebApp01.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class BrandController : Controller
+    {
+        private readonly DataContext _dataContext;
+        public BrandController(DataContext context)
+        {
+            _dataContext = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _dataContext.Brands.OrderByDescending(p => p.Id).ToListAsync());
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public async Task<IActionResult> Create(BrandModel brand)
+        {
+
+            if (ModelState.IsValid) // neu du lieu nhập vào đúng với Model
+            {
+                // them du lieu
+                //TempData["success"] = "OK";
+                brand.Slug = brand.Name.Replace(" ", "-");
+                var slug = await _dataContext.Brands.FirstOrDefaultAsync(p => p.Slug == brand.Slug);
+                if (slug != null)
+                {
+                    ModelState.AddModelError("", "Nhà xuất bản đã có sẵn");
+                    return View(brand);
+                }
+
+                _dataContext.Add(brand);
+                await _dataContext.SaveChangesAsync();
+                TempData["success"] = "Đã thêm thành công";
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                TempData["error"] = "Có lỗi xảy ra";
+                List<string> errors = new List<string>();
+                foreach (var value in ModelState.Values)
+                {
+                    foreach (var error in value.Errors)
+                    {
+                        errors.Add(error.ErrorMessage);
+                    }
+                }
+                string errorMessage = string.Join("\n", errors);
+                return BadRequest(errorMessage);
+            }
+
+            return View(brand);
+        }
+
+        public async Task<IActionResult> Edit(int Id)
+        {
+            BrandModel brand = await _dataContext.Brands.FindAsync(Id);
+            return View(brand);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public async Task<IActionResult> Edit(BrandModel brand)
+        {
+
+            if (ModelState.IsValid) // neu du lieu nhập vào đúng với Model
+            {
+                // them du lieu
+                //TempData["success"] = "OK";
+                brand.Slug = brand.Name.Replace(" ", "-");
+                var slug = await _dataContext.Brands.FirstOrDefaultAsync(p => p.Slug == brand.Slug);
+                if (slug != null)
+                {
+                    ModelState.AddModelError("", "NXB đã có sẵn");
+                    return View(brand);
+                }
+
+                _dataContext.Update(brand);
+                await _dataContext.SaveChangesAsync();
+                TempData["success"] = "Đã cập nhật thành công";
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                TempData["error"] = "Có lỗi xảy ra";
+                List<string> errors = new List<string>();
+                foreach (var value in ModelState.Values)
+                {
+                    foreach (var error in value.Errors)
+                    {
+                        errors.Add(error.ErrorMessage);
+                    }
+                }
+                string errorMessage = string.Join("\n", errors);
+                return BadRequest(errorMessage);
+            }
+
+            return View(brand);
+        }
+
+        public async Task<IActionResult> Delete(int Id)
+        {
+            BrandModel brand = await _dataContext.Brands.FindAsync(Id);
+
+            _dataContext.Brands.Remove(brand);
+            await _dataContext.SaveChangesAsync();
+            TempData["error"] = "Đã xóa NXB";
+            return RedirectToAction("Index");
+        }
+
+
+    }
+}
