6a91157e48c3a1c62646f04c15e3eed2ee2ae32d
Agile101hcmue <89836403+TrHgTung@users.noreply.github.com>
2023-11-26 16:05:39 +0700

    Admin logout

---
+++ Areas/Admin/Controllers/AccountController.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using WebApp01.Models;
+using WebApp01.Models.ViewModels;
+
+namespace WebApp01.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = "Admin")]
+    public class AccountController : Controller
+    {
+        private UserManager<AppUserModel> _userManager;
+        private SignInManager<AppUserModel> _signInManager;
+        public AccountController(SignInManager<AppUserModel> signInManager, UserManager<AppUserModel> userManager)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+        }
+        public IActionResult Login(string returnUrl)
+        {
+            return View(new LoginViewModel { ReturnUrl = returnUrl });
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel loginVM)
+        {
+            if (ModelState.IsValid)
+            {
+                Microsoft.AspNetCore.Identity.SignInResult result = await _signInManager.PasswordSignInAsync(loginVM.Username, loginVM.Password, false, false);
+                if (result.Succeeded)
+                {
+                    return Redirect(loginVM.ReturnUrl ?? "/"); // success -> url ; false -> return index (login)
+                }
+                ModelState.AddModelError("", "Sai thông tin đăng nhập");
+            }
+            return View(loginVM);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(UserModel user)
+        {
+            if (ModelState.IsValid)
+            {
+                AppUserModel newUser = new AppUserModel { UserName = user.Username, Email = user.Email };
+                IdentityResult result = await _userManager.CreateAsync(newUser, user.Password);
+                if (result.Succeeded)
+                {
+                    TempData["success"] = "Tạo tài khoản thành công";
+                    return Redirect("/account/login");
+                }
+                foreach (IdentityError error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+            }
+            return View(user);
+        }
+
+        public async Task<IActionResult> Logout(string returnUrl = "/")
+        {
+            await _signInManager.SignOutAsync();
+
+            return Redirect("/");
+        }
+    }
+}
