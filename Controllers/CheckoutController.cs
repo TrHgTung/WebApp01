4329932ac7d7c6b38cd81df8104c46378a993a05
Agile101hcmue <89836403+TrHgTung@users.noreply.github.com>
2023-12-04 03:46:10 +0700

    test env

---
+++ Controllers/CheckoutController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp01.Controllers
+{
+    public class CheckoutController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
